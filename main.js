da4e16b1525d67dc35570e06b101cb9e156a997f
ElionEriksson <64314315+ElionEriksson@users.noreply.github.com>
2020-11-13 16:43:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function elions_func() {
-    const elion = document.getElementsByClassName("container-1");
-    elion.style.display = none;
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('clck', () => {
+        nav.classList.toggle('nav-active');
+    });
 }
+
+const app = () => {
+    navslide();
+}
+
+// https://www.youtube.com/watch?v=gXkqy0b4M5g
